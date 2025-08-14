34a9ac9dbaf38e671f58657633269398e5e3f5de
Hussain <hkosaimi@gmail.com>
2025-08-14 11:17:49 +0300

    new

--- server.js
+++ server.js
@@ -29,7 +29,12 @@ const app = express();
 // CORS
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173", "https://admin-1-qw2b.onrender.com"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://admin-1-qw2b.onrender.com",
+      "https://admin-production-ce84.up.railway.app/",
+    ],
     credentials: true,
   })
 );
