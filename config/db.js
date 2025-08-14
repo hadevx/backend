9772856f7c6ea9370c371e4e675162da6ce528a6
Hussain <hkosaimi@gmail.com>
2025-08-14 10:14:35 +0300

    fixing DB

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
   } catch (error) {
     console.log(`Data Base Error: ${error.message}`);
     process.exit(1);
