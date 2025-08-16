0ae0b34aae86ff569539127cdd6f1e5031d4840f
Hussain <hkosaimi@gmail.com>
2025-08-16 05:53:09 +0300

    new

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { protectUser, protectAdmin } = require("../middleware/authMiddleware");
 const registerLimiter = require("../utils/registerLimit");
 const {
   loginUser,
@@ -13,16 +14,15 @@ const {
   updateUser,
   createAddress,
   getAddress,
+  updateAddress,
   loginAdmin,
   forgetPassword,
   resetPassword,
 } = require("../controllers/userController");
 
-const { protectUser, protectAdmin } = require("../middleware/authMiddleware");
-
 /* http://localhost:4001/api/users */
 
-// ----------------- Client / Storefront Routes -----------------
+// Client routes
 router.post("/login", loginUser);
 router.post("/register", registerLimiter, registerUser);
 router.get("/address/:userId", protectUser, getAddress);
@@ -30,16 +30,15 @@ router.post("/address", protectUser, createAddress);
 router.put("/address", protectUser, updateAddress);
 router.get("/profile", protectUser, getUserProfile);
 router.put("/profile", protectUser, updateUserProfile);
-router.post("/logout", protectUser, logoutUser);
+router.post("/logout", logoutUser);
 
-// ----------------- Admin Routes -----------------
-router.post("/admin/login", loginAdmin); // Admin login
-router.get("/", protectAdmin, getUsers);
-router.put("/:id", protectAdmin, updateUser);
-router.delete("/:id", protectAdmin, deleteUser);
-router.get("/:id", protectAdmin, getUserById);
+// Admin routes
+router.get("/", protectUser, protectAdmin, getUsers);
+router.put("/:id", protectUser, protectAdmin, updateUser);
+router.delete("/:id", protectUser, protectAdmin, deleteUser);
+router.get("/:id", protectUser, protectAdmin, getUserById);
+router.post("/admin", loginAdmin);
 
-// ----------------- Password Reset Routes -----------------
 router.post("/forget-password", forgetPassword);
 router.post("/reset-password/:token", resetPassword);
 
