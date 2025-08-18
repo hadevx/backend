d1963987b8d32c3556a814d4ee139f85e1c345a8
Hussain <hkosaimi@gmail.com>
2025-08-18 16:28:43 +0300

    new

--- controllers/productController.js
+++ controllers/productController.js
@@ -380,7 +380,7 @@ const getCategories = asyncHandler(async (req, res) => {
   }
   res.json(deleteCategory);
 }); */
-const deleteCategory = asyncHandler(async (req, res) => {
+/* const deleteCategory = asyncHandler(async (req, res) => {
   const name = req.query.name; // get name from query
   const deletedCategory = await Category.findOneAndDelete({ name });
   if (!deletedCategory) {
@@ -388,7 +388,19 @@ const deleteCategory = asyncHandler(async (req, res) => {
     throw new Error("Category not found");
   }
   res.json(deletedCategory);
+}); */
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.body; // now expecting { id: "categoryId" }
+
+  const deletedCategory = await Category.findByIdAndDelete(id);
+  if (!deletedCategory) {
+    res.status(404);
+    throw new Error("Category not found");
+  }
+
+  res.json(deletedCategory);
 });
+
 module.exports = {
   getProducts,
   getProductById,
