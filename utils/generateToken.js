6a0d85db04b5f84561d7ce3ada217b993d8d67d1
Hussain <hkosaimi@gmail.com>
2025-08-19 10:52:26 +0300

    new

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+/* const jwt = require("jsonwebtoken");
 
 const generateToken = (res, userId, user) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
@@ -11,6 +11,27 @@ const generateToken = (res, userId, user) => {
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
     maxAge: 1 * 24 * 60 * 60 * 1000,
+  });
+};
+
+module.exports = generateToken;
+ */
+const jwt = require("jsonwebtoken");
+
+const generateToken = (res, user) => {
+  // Add role info into payload
+  const token = jwt.sign(
+    { userId: user._id, role: user.isAdmin ? "admin" : "user" },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  // Always one cookie name
+  res.cookie("jwt", token, {
+    httpOnly: true, // The cookie cannot be accessed by JavaScript running in the browser
+    secure: process.env.NODE_ENV !== "development", // secure in production
+    sameSite: "strict",
+    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
   });
 };
 
