9ee4b94d3973e673a59dda612b53ddd1a2bf6b03
Hussain <hkosaimi@gmail.com>
2025-08-23 21:27:46 +0300

    fixing cookies

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -22,20 +22,20 @@ const generateToken = (res, user, subdomain) => {
   const role = user.isAdmin ? "admin" : "user";
   const token = jwt.sign({ userId: user._id, role }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
-  let domain = undefined;
+  /*   let domain = undefined;
 
   if (subdomain === "storefront") {
     domain = "storefront.webschema.online";
   }
   if (subdomain === "admin") {
     domain = "admin.webschema.online";
-  }
+  } */
 
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
-    // domain: "admin.webschema.online",
+    domain: "admin.webschema.online",
     maxAge: 1 * 24 * 60 * 60 * 1000,
   };
 
