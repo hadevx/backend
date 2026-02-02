3423d91af26f9bf8b67a92214e75374983ce8d6f
Hussain <hkosaimi@gmail.com>
2026-02-02 15:31:03 +0300

    new

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,17 +4,10 @@ const generateToken = (res, user) => {
   const role = user.isAdmin ? "admin" : "user";
   const token = jwt.sign({ userId: user._id, role }, process.env.JWT_SECRET, { expiresIn: "5d" });
 
-  /*   const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 5 * 24 * 60 * 60 * 1000,
-  }; */
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    domain: req.hostname, // storefront.webschema.online OR admin.webschema.online
+    sameSite: "strict",
     maxAge: 5 * 24 * 60 * 60 * 1000,
   };
 
